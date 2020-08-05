b8880e080b3be58c0e33019e3a94b4f77e747bb7
thomas.davidson.18 <thomas.davidson.18@cnu.edu>
2020-08-05 16:19:16 -0400

    added anyfin can happen calculator.

--- HSlib.py
+++ HSlib.py
@@ -1,5 +1,42 @@
+import random
 
 tri = lambda x: int(x * (x+1)) //2
+
+def anyfinDMG():
+    """
+    this method will determine the damage from casting anyfin can happen with percentages that you get at
+    leas that amount of damage.
+    in the mathod it asks for you to imput "war" for warleader, "old" for old murkeye,
+    "blue" for bluegill, "lil" for grimscale oracle, and "dud"  refers to any dead murlocs that were not in this list
+    percentages in this method are determined with monty carlo simulations of seize 4000.
+    :return: None
+    """
+    W, O, B, L, D, S = [int(x) for x in input("war old blue lil dud minSpace\n").split()]
+
+    temp = ["w"] * W + ["o"] * O + ["b"] * B + ["l"] * L + ["d"] * D
+    for room in range(S, 8):
+        dmghist = {}
+        for _ in range(4000):
+            random.shuffle(temp)
+            temphist = temp[:room]
+            w = sum(["w" == x for x in temphist])
+            o = sum(["o" == x for x in temphist])
+            b = sum(["b" == x for x in temphist])
+            l = sum(["l" == x for x in temphist])
+            d = sum(["d" == x for x in temphist])
+            dmg = (2 + (2 * w) + l) * (b + o) + (len(temphist) - 1) * o
+            if dmg not in dmghist:
+                dmghist[dmg] = .025
+                continue
+            dmghist[dmg] += .025
+
+        print(room, "space we get.")
+        dmgsort = sorted(list(dmghist), key=lambda x: -x)
+        chance = 0
+        for key in dmgsort:
+            chance += dmghist[key]
+            print("{:3d} damage: {:3.2f}%".format(key, chance))
+        print("\n##############\n")
 
 
 def fatigue(drawn, current=1):
